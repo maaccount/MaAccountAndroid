f95dbcd9503de7ad00fb7b913302ff83189cfc41
Auroral <xulin@vickn.com>
2019-03-13 02:47:15 +0800

    项目初始化

---
+++ main/src/main/java/com/xyz/maaccount/main/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.xyz.maaccount.main;
+
+import android.os.Bundle;
+
+import com.xyz.maaccount.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
